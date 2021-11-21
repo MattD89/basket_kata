6d19e3376f0f3749f293d59f0e66362690669b02
Matt Dyer <mattdyer_graphics@hotmail.co.uk>
2021-11-21 12:18:18 +0000

    first commit

---
+++ BasketKata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasketKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
