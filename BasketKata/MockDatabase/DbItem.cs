c5b079fb3d51ff12d44d0a6a3c39793f11062832
Matt Dyer <mattdyer_graphics@hotmail.co.uk>
2021-11-21 12:45:13 +0000

    Added mock database call for Items

--- BasketKata/MockDatabase/DbItem.cs
+++ BasketKata/MockDatabase/DbItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BasketKata.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,14 +7,47 @@ namespace BasketKata.MockDatabase
 {
     class DbItem
     {
-        //public DbItem()
-        //{
+        public List<Item> Items = new List<Item>();
+        public DbItem()
+        {
+            GetItemTypes();
+        }
 
-        //}
+        public void GetItemTypes()
+        {
+            var items = new List<Item>();
 
-        //public void GetItems()
-        //{
+            Item itemA = new Item()
+            {
+                Id = 1,
+                SKU = "A",
+                HasPromotion = false
+            };
+            items.Add(itemA);
 
-        //}
+            Item itemB = new Item()
+            {
+                Id = 2,
+                SKU = "B",
+                HasPromotion = true
+            };
+            items.Add(itemB);
+
+            Item itemC = new Item()
+            {
+                Id = 3,
+                SKU = "C",
+                HasPromotion = false
+            };
+            items.Add(itemC);
+
+            Item itemD = new Item()
+            {
+                Id = 4,
+                SKU = "D",
+                HasPromotion = true
+            };
+            items.Add(itemD);
+        }
     }
 }
